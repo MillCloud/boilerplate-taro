49b96c2fe4433dc8681c7d07a766bff268352e18
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-05-24 08:08:52 +0800

    chore: initial

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = ({ children }: { children: React.ReactNode }) => children;
+
+export default App;
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from '@tarojs/components';
+
+const Index: React.FC = (props) => (
+  <View className="index">
+    <Text>Hello world!</Text>
+  </View>
+);
+
+export default Index;
